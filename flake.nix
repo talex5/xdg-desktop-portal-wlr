3adc6fccb264e5ed9726ec13d3891f3b8f675c3e
Thomas Leonard <talex5@gmail.com>
2023-02-14 12:39:06 +0000

    Build with Nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = import ./. pkgs;
+      }
+    );
+}
